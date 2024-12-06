3a5e6233ec11ef607d61f14408995019000e6f03
DimpleKA <153168007+DimpleKA@users.noreply.github.com>
2024-12-06 10:08:24 +0530

    sdf

--- src/Components/Outhzero/LogoutButton.jsx
+++ src/Components/Outhzero/LogoutButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch } from "react-redux";
-import { logout } from "../store/loginSlice"; // Import logout action
+import { logout } from "../../features/loginSlice"; // Import logout action
 
 const LogoutButton = () => {
   const { logout: authLogout } = useAuth0();
