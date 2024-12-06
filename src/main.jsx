c4b1b5dfeed0281590ce26e6dba196842bcf5847
DimpleKA <153168007+DimpleKA@users.noreply.github.com>
2024-12-06 06:23:33 +0530

    sdf

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Auth0Provider
+  <Auth0Provider
     domain="dev-farcynnuiewz53gu.us.auth0.com"
-    clientId="hYoTpBTDIw0hwL5FQexgcBmHIhCGWs7l"
+    clientId="d5ZrVgItD0TsAnlpbWUqlQZ4s3CRnHtv"
     authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: window.location.origin
     }}
   >
 
